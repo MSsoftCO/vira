9b1b9cd94d77e5c8a649dce257fd36ea998fa694
Morteza <mssoftco@gmail.com>
2019-08-07 13:11:55 +0430

    Prepare to deploy

---
+++ docs/precache-manifest.29863464c189feabaad52970c0bb2d6b.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5987cfd2af6fd44b845c7005d2863b9d",
+    "url": "/vira/index.html"
+  },
+  {
+    "revision": "82d973e8e99f9c3d7c3a",
+    "url": "/vira/static/css/2.5d920484.chunk.css"
+  },
+  {
+    "revision": "105a304e65dc338ecf0f",
+    "url": "/vira/static/css/main.d59c62a4.chunk.css"
+  },
+  {
+    "revision": "82d973e8e99f9c3d7c3a",
+    "url": "/vira/static/js/2.4b8d675d.chunk.js"
+  },
+  {
+    "revision": "105a304e65dc338ecf0f",
+    "url": "/vira/static/js/main.13fea0b8.chunk.js"
+  },
+  {
+    "revision": "1a096b1daaf3cfe463f5",
+    "url": "/vira/static/js/runtime~main.7973135a.js"
+  },
+  {
+    "revision": "0ae68fe9a34451462b4a917bbb84e5ef",
+    "url": "/vira/static/media/Vazir-Black.0ae68fe9.eot"
+  },
+  {
+    "revision": "163edc9f0e77cdd5f2d8fd60b3b83406",
+    "url": "/vira/static/media/Vazir-Black.163edc9f.woff2"
+  },
+  {
+    "revision": "5667ccbb024f56b7501fbefe80909c3c",
+    "url": "/vira/static/media/Vazir-Black.5667ccbb.woff"
+  },
+  {
+    "revision": "fa89dbf0e6b7e107f7209d36aa4a5190",
+    "url": "/vira/static/media/Vazir-Black.fa89dbf0.ttf"
+  },
+  {
+    "revision": "4774ad8ec818ee81fc470ecdc3e8831f",
+    "url": "/vira/static/media/Vazir-Bold.4774ad8e.eot"
+  },
+  {
+    "revision": "51bd611abab32dd3f1659d054b290b6f",
+    "url": "/vira/static/media/Vazir-Bold.51bd611a.woff2"
+  },
+  {
+    "revision": "56dce27ac716170e603286ba0d440de0",
+    "url": "/vira/static/media/Vazir-Bold.56dce27a.woff"
+  },
+  {
+    "revision": "9ec1b4dd9a4d4916ed17ecf952552e66",
+    "url": "/vira/static/media/Vazir-Bold.9ec1b4dd.ttf"
+  },
+  {
+    "revision": "122071db7286d40324cb22c75ebac1f8",
+    "url": "/vira/static/media/Vazir-Light.122071db.eot"
+  },
+  {
+    "revision": "d5bcad0cb424cc2a7cca1e9178fdecf9",
+    "url": "/vira/static/media/Vazir-Light.d5bcad0c.woff2"
+  },
+  {
+    "revision": "ecdfbb9ddc7b46dba62f7ba73e84bfea",
+    "url": "/vira/static/media/Vazir-Light.ecdfbb9d.ttf"
+  },
+  {
+    "revision": "eee1fa58b4f97ccfe668954c67571f2f",
+    "url": "/vira/static/media/Vazir-Light.eee1fa58.woff"
+  },
+  {
+    "revision": "01e07ecc7ef15aac97ad543c46500b73",
+    "url": "/vira/static/media/Vazir-Medium.01e07ecc.woff"
+  },
+  {
+    "revision": "14d12966397606370e52017e5312887b",
+    "url": "/vira/static/media/Vazir-Medium.14d12966.woff2"
+  },
+  {
+    "revision": "9f8a7b6da82ed90759a1cd03477a0b91",
+    "url": "/vira/static/media/Vazir-Medium.9f8a7b6d.eot"
+  },
+  {
+    "revision": "d9369334a85109c002f0093a8bbb5990",
+    "url": "/vira/static/media/Vazir-Medium.d9369334.ttf"
+  },
+  {
+    "revision": "731561a6ca3e2b4aa76f152a4b534fb1",
+    "url": "/vira/static/media/Vazir-Thin.731561a6.woff"
+  },
+  {
+    "revision": "7a0b4950d20ea06f783184e89f5aae2f",
+    "url": "/vira/static/media/Vazir-Thin.7a0b4950.ttf"
+  },
+  {
+    "revision": "9c24f84ae131313dda7e723dffe280ad",
+    "url": "/vira/static/media/Vazir-Thin.9c24f84a.woff2"
+  },
+  {
+    "revision": "cddbd4ba7244b014884f772823b7a93a",
+    "url": "/vira/static/media/Vazir-Thin.cddbd4ba.eot"
+  },
+  {
+    "revision": "03ed11c59bca0b7f21f27cbb86a09c20",
+    "url": "/vira/static/media/Vazir.03ed11c5.woff"
+  },
+  {
+    "revision": "7e32f522be5c69507074619fcaa7fff5",
+    "url": "/vira/static/media/Vazir.7e32f522.eot"
+  },
+  {
+    "revision": "b295a26db726e74748c7280264b5d5d4",
+    "url": "/vira/static/media/Vazir.b295a26d.ttf"
+  },
+  {
+    "revision": "c353f7ef8a50a4c7c282c2b30250738c",
+    "url": "/vira/static/media/Vazir.c353f7ef.woff2"
+  },
+  {
+    "revision": "b7c508aae16be1448f25e433201656e7",
+    "url": "/vira/static/media/arsenal-logo.b7c508aa.png"
+  },
+  {
+    "revision": "b4ae758523f175e09d5636cf1bf0fe46",
+    "url": "/vira/static/media/chelsea-logo.b4ae7585.png"
+  },
+  {
+    "revision": "bf42104411415383faf9a4bb9721aa31",
+    "url": "/vira/static/media/everton-logo.bf421044.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vira/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vira/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vira/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vira/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vira/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1086e9fdfdb72e8dba4fa12fb4a6b21b",
+    "url": "/vira/static/media/liverpool-logo.1086e9fd.png"
+  },
+  {
+    "revision": "c4913e1b5cf91345566dc7a92d4d1124",
+    "url": "/vira/static/media/manchester-city-logo.c4913e1b.png"
+  },
+  {
+    "revision": "d205c32af95b84ab1f3f34a4d6e9991c",
+    "url": "/vira/static/media/manchester-united-logo.d205c32a.png"
+  },
+  {
+    "revision": "16d1bc6c826073856e63b5453b5dc2cc",
+    "url": "/vira/static/media/masod-ozil.16d1bc6c.jpg"
+  }
+]);
